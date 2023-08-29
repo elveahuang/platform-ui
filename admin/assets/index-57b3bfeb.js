fedac5c6bbdc7c62d7eed70edd911164dac9b271
elveahuang <elvea.huang@gmail.com>
2023-08-29 11:21:20 +0000

    Deploying to gh-pages from @ elveahuang/platform-ui-vue@4cdeb23c05bb8bf5664bf975bfd5139f55200f98 🚀

--- admin/assets/index-57b3bfeb.js
+++
@@ -1,1 +0,0 @@
-import{d as t,c as e,a as n}from"./index-e296b19c.js";const d=t({name:"Index",setup(){return()=>e("div",null,[e("div",{class:"text-center"},[n("Index")])])}});export{d as default};
---
+++ admin/assets/workbench-9cd17fbd.js
@@ -0,0 +1,1 @@
+import{d as t,c as e,a}from"./index-4d3b2731.js";const r=t({name:"ProductDetailsPage",setup(){return()=>e("div",null,[e("div",{class:"text-center"},[a("ProductDetailsPage")])])}});export{r as default};
