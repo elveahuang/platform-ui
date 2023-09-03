064a59b3d1c135023a478bc1daedd5e1c55e161b
elveahuang <elvea.huang@gmail.com>
2023-09-03 15:35:39 +0000

    Deploying to gh-pages from @ elveahuang/platform-ui-vue@88caa01757cacf9f2fdcd057cd91d9e712309c1a 🚀

--- mobile/assets/redirect-3161569c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{a4 as t,a5 as c,a6 as r}from"./index-da393970.js";const o={},s=r("div",{class:"text-center"},"Redirect",-1),a=[s];function n(_,d){return t(),c("div",null,a)}const l=e(o,[["render",n]]);export{l as default};
