56bf69d0629e517944743468027c804b9b5fb17a
elveahuang <elvea.huang@gmail.com>
2023-12-14 12:43:27 +0000

    Deploying to gh-pages from @ elveahuang/platform-ui-vue@96f434295ca7c4e1cfa16620cf1c08cca81eee63 🚀

---
+++ admin/assets/refs-xbtUysNt.js
@@ -0,0 +1,1 @@
+import{b0 as t}from"./index-l0w7rwc5.js";const e=(...c)=>s=>{c.forEach(o=>{t(o)?o(s):o.value=s})};export{e as c};
---
+++ admin/assets/user-HBMouxRp.js
@@ -0,0 +1,1 @@
+import{v as a}from"./index-l0w7rwc5.js";const o=s=>a("/api/v1/user/account",s),t=s=>a("/api/v1/user/change-password",s);export{t as c,o as u};
