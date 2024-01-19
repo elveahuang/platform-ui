551ae219198e628fb3776b5e0a964ff843264236
elveahuang <elvea.huang@gmail.com>
2024-01-19 07:31:43 +0000

    Deploying to gh-pages from @ elveahuang/platform-ui@ea9b3afc7fc0afe0f9a02e31137f9f79d92983c4 🚀

--- admin/assets/user-F3J3MWL9.js
+++
@@ -1,1 +0,0 @@
-import{v as a}from"./index-OstrK1OQ.js";const o=s=>a("/api/v1/user/account",s),t=s=>a("/api/v1/user/change-password",s);export{t as c,o as u};
---
+++ webapp/assets/user-BEv5L-fS.js
@@ -0,0 +1,1 @@
+import{a9 as s}from"./index-I4XNYiL7.js";const o=a=>s("/api/v1/user/account",a),t=a=>s("/api/v1/user/change-password",a);export{t as c,o as u};
