d4cc51913c4c8258c0459a9b592f6bbafe85c759
elveahuang <elvea.huang@gmail.com>
2024-06-11 03:37:19 +0000

    Deploying to gh-pages from @ elveahuang/ui@dabb61200e2e9c157bde6b5f4dc27c3f1621a1a0 🚀

---
+++ admin/assets/index-CgUB16AP.js
@@ -0,0 +1,1 @@
+import{_ as e,e as c,g as n}from"./index-DzxqvCZ5.js";const r={};function t(o,s){return c(),n("div")}const a=e(r,[["render",t]]);export{a as default};
---
+++ mobile/assets/notice-DeADjwr9.js
@@ -0,0 +1,1 @@
+import{ar as i}from"./index-CLZvx8Ks.js";const s=t=>i("/api/v1/notice/list",t),a=t=>i("/api/v1/notice/details",t);export{a,s as n};
